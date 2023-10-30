7e4d2e81000c8374dc681ba38b4505db9acb9750
HungNT1st <132951108+HungNT1st@users.noreply.github.com>
2023-10-29 22:47:50 -0400

    Responsive footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,44 +1,123 @@
-import { Box } from '@chakra-ui/react'
-import { Text } from '@chakra-ui/react'
-import { Flex } from '@chakra-ui/react'
-function Footer() {
-    return (
-        <footer className="App-Footer">   
-                    <Box pb="10px"bg="#FAFAFA"alignItems="center"justifyContent="center">
+'use client'
 
-                        <Box marginLeft="50px"marginRight="50px"pb="20px"bg="white"alignItems="center"justifyContent="center">
-                        <Flex justifyContent="space-between">
-                        <Text ml="40px">
-                            Get in Touch
-                                
-                        </Text>
-                        <Text>
-                            Company Info
-                            
-                        </Text>
-                        <Text>
-                            Features
-                            
-                        </Text>
-                        <Text mr="40px">
-                            Resources
-                            
-                        </Text>
-                        </Flex>
-                        </Box>
-                    
-                    
-                            <Box alignItems="center"justifyContent="center"display="flex">
-                                
-                            <Text color="gray">
-                            Made by CS320 Students copyright 2023
-                            </Text>
-                            </Box>
-                    
-                    </Box>
+import { ReactNode } from 'react'
+import {
+  Box,
+  Container,
+  Stack,
+  SimpleGrid,
+  Text,
+  VisuallyHidden,
+  chakra,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa'
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  )
+}
 
-         </footer>
-    );
-  }
-  
-  export default Footer;
+const SocialButton = ({
+  children,
+  label,
+  href,
+}) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  )
+}
+
+export default function LargeWithAppLinksAndSocial() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}>
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+            <Stack align={'flex-start'}>
+                <ListHeader>Get In Touch</ListHeader>
+                <Stack direction={'row'} spacing={6}>
+                <SocialButton label={'Twitter'} href={'#'}>
+                <FaTwitter />
+                </SocialButton>
+                <SocialButton label={'YouTube'} href={'#'}>
+                <FaYoutube />
+                </SocialButton>
+                <SocialButton label={'Instagram'} href={'#'}>
+                <FaInstagram />
+                </SocialButton>
+                </Stack>
+            </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Company</ListHeader>
+            <Box as="a" href={'#'}>
+              About Us
+            </Box>
+            <Box as="a" href={'#'}>
+              Blog
+            </Box>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Features</ListHeader>
+            <Box as="a" href={'#'}>
+              Business Marketing
+            </Box>
+            <Box as="a" href={'#'}>
+              User Analytic
+            </Box>
+            <Box as="a" href={'#'}>
+              Live Chat
+            </Box>
+            <Box as="a" href={'#'}>
+              Unlimited Support
+            </Box>
+          </Stack>
+
+          <Stack align={'flex-start'}>
+            <ListHeader>Resources</ListHeader>
+            <Box as="a" href={'#'}>
+              Ios & Android App
+            </Box>
+            <Box as="a" href={'#'}>
+              Watch a Demo
+            </Box>
+            <Box as="a" href={'#'}>
+              Customers
+            </Box>
+            <Box as="a" href={'#'}>
+              API
+            </Box>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+
+      <Box alignItems="center"justifyContent="center"display="flex">
+        <Text color="gray">
+            Made by CS320 Students copyright 2023
+       </Text>
+      </Box>
+    </Box>
+  )
+}
