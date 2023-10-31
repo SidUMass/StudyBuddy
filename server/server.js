a424bdabe35995db9c3a7ccd45117cbc3c468580
atrehan0803 <trehananushka0803@gmail.com>
2023-10-31 00:10:49 -0400

    second push

--- server/server.js
+++ server/server.js
@@ -1,44 +1,51 @@
+
 const express = require('express');
+const bcrypt = require('bcrypt'); 
+// using bcrypt for now to hash the user's password
+const pgp = require('pg-promise')();
+// usin g pg promise for postgres here
+
 const app = express();
-const bodyParser = require('body-parser');
-const { Pool } = require('pg');
 const PORT = 5000;
 
-app.use(bodyParser.json());
+// sanjana, can you add credentials here before using it?
+// also, use this query to create the users table for now:
 
-const pool = new Pool({
-  user: 'your_username',
-  host: 'your_host',
-  database: 'your_database',
-  password: 'your_password',
-  port: 5432, // Replace with your PostgreSQL port
-});
+// CREATE TABLE users (
+//   id SERIAL PRIMARY KEY,
+//   username VARCHAR(255) UNIQUE NOT NULL,
+//   email VARCHAR(255) NOT NULL,
+//   password VARCHAR(255) NOT NULL
+// );
 
-pool.connect()
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch((err) => {
-    console.error('Error connecting to the database', err);
-  });
+const db = pgp('postgres://username:password@localhost:5432/your_database_name');
+app.use(express.json()); // Middleware for parsing JSON data from the request body
 
-// Create a new user
-app.post('/api/createUser', async (req, res) => {
-  const { id, email, password } = req.body;
-
-  if (!id || !email || !password) {
-    return res.status(400).json({ message: 'Please provide valid user information' });
+app.post('/api/register', async (req, res) => {
+  const { username, password, email } = req.body;
+  if (!username || !password || !email) {
+    return res.json({ success: false, message: 'Please provide all required fields.' });
   }
-
   try {
-    await pool.query('INSERT INTO users (id, email, password) VALUES ($1, $2, $3)', [id, email, password]);
-    res.status(201).json({ message: 'User created successfully' });
+    const userExists = await db.oneOrNone('SELECT id FROM users WHERE username = $1', [username]);
+    if (userExists) {
+      return res.json({ success: false, message: 'Username already exists.' });
+    }
+    // apparently this stores the password with an encryption, thought it was useful so i added it here.
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.none('INSERT INTO users(username, email, password) VALUES($1, $2, $3)', [username, email, hashedPassword]);
+    res.json({ success: true, message: 'Registration successful.' });
   } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ message: 'User registration failed' });
+    console.error('Error registering user:', error);
+    res.json({ success: false, message: 'There was an error during registration.' });
   }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from the server!');
+// });
+
