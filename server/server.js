29b5616052b06fb76df8caecba832dfe3995163d
Sanjana Sitaraman <sanjanasitar@umass.edu>
2023-10-30 23:56:38 -0400

    added user input

--- server/server.js
+++ server/server.js
@@ -1,41 +1,42 @@
 const express = require('express');
-const { Client } = require('pg');
 const app = express();
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 const PORT = 5000;
 
-const client = new Client({
-  user: 'postgres',
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  user: 'your_username',
   host: 'your_host',
   database: 'your_database',
   password: 'your_password',
-  port: 5432, // Change to your PostgreSQL port if different
+  port: 5432, // Replace with your PostgreSQL port
 });
 
-// Connect to the PostgreSQL database
-client.connect()
+pool.connect()
   .then(() => {
     console.log('Connected to the database');
-
-    // Define the SQL query
-    const query = {
-      text: 'INSERT INTO users(id, role, name, email) VALUES($1, $2, $3, $4)',
-      values: [1, 'professor', 'Jaime', null]
-    };
-
-    return client.query(query);
   })
-  .then((result) => {
-    console.log('User created successfully');
-  })
-  .catch((error) => {
-    console.error('Error creating user:', error);
-  })
-  .finally(() => {
-    client.end();
+  .catch((err) => {
+    console.error('Error connecting to the database', err);
   });
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+// Create a new user
+app.post('/api/createUser', async (req, res) => {
+  const { id, email, password } = req.body;
+
+  if (!id || !email || !password) {
+    return res.status(400).json({ message: 'Please provide valid user information' });
+  }
+
+  try {
+    await pool.query('INSERT INTO users (id, email, password) VALUES ($1, $2, $3)', [id, email, password]);
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ message: 'User registration failed' });
+  }
 });
 
 app.listen(PORT, () => {
