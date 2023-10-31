f478cbb212577d818b0da2474bbfec95a8d873c0
Sanjana Sitaraman <sanjanasitar@umass.edu>
2023-10-30 23:35:05 -0400

    sql create user query

--- server/server.js
+++ server/server.js
@@ -1,6 +1,38 @@
 const express = require('express');
+const { Client } = require('pg');
 const app = express();
 const PORT = 5000;
+
+const client = new Client({
+  user: 'postgres',
+  host: 'your_host',
+  database: 'your_database',
+  password: 'your_password',
+  port: 5432, // Change to your PostgreSQL port if different
+});
+
+// Connect to the PostgreSQL database
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+
+    // Define the SQL query
+    const query = {
+      text: 'INSERT INTO users(id, role, name, email) VALUES($1, $2, $3, $4)',
+      values: [1, 'professor', 'Jaime', null]
+    };
+
+    return client.query(query);
+  })
+  .then((result) => {
+    console.log('User created successfully');
+  })
+  .catch((error) => {
+    console.error('Error creating user:', error);
+  })
+  .finally(() => {
+    client.end();
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
