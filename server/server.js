685b989fbe788b9e25fa100d912b8cba9210ba3a
atrehan0803 <trehananushka0803@gmail.com>
2023-10-31 20:17:58 -0400

    Anushka's backend changes

--- server/server.js
+++ server/server.js
@@ -1,29 +1,34 @@
 
 const express = require('express');
 const bcrypt = require('bcrypt'); 
-// using bcrypt for now to hash the user's password
 const { Pool } = require('pg');
-// usin g pg promise for postgres here
 
 const app = express();
 const PORT = 5000;
 
-const db = pgp('postgres://postgres:Ihtirms#123@localhost:5432/your_database_name');
-app.use(express.json()); // Middleware for parsing JSON data from the request body
+// Replace the following credentials with your own
+const pool = new Pool({
+  user: 'your_username',
+  host: 'localhost',
+  database: 'your_database_name',
+  password: 'your_password',
+  port: 5432,
+});
+
+app.use(express.json());
 
 app.post('/api/register', async (req, res) => {
-  const { username, password, email } = req.body;  
+  const { username, password, email } = req.body;
   if (!username || !password || !email) {
     return res.json({ success: false, message: 'Please provide all required fields.' });
   }
   try {
-    const userExists = await db.oneOrNone('SELECT id FROM users WHERE username = $1', [username]);
-    if (userExists) {
+    const userExists = await pool.query('SELECT id FROM users WHERE username = $1', [username]);
+    if (userExists.rows.length) {
       return res.json({ success: false, message: 'Username already exists.' });
     }
-    // apparently this stores the password with an encryption, thought it was useful so i added it here.
     const hashedPassword = await bcrypt.hash(password, 10);
-    await db.none('INSERT INTO users(username, email, password) VALUES($1, $2, $3)', [username, email, hashedPassword]);
+    await pool.query('INSERT INTO users(username, email, password) VALUES($1, $2, $3)', [username, email, hashedPassword]);
     res.json({ success: true, message: 'Registration successful.' });
   } catch (error) {
     console.error('Error registering user:', error);
@@ -34,8 +39,3 @@ app.post('/api/register', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello from the server!');
-// });
-
