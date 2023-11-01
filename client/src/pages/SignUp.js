0de6c15341daa4a27599bf6420da6f5d7970d443
Sanjana Sitaraman <sanjanasitar@umass.edu>
2023-10-31 21:18:17 -0400

    database connected

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -22,7 +22,7 @@ function SignUp() {
     setMessage(''); // Clear any previous messages
     
     try {
-      const response = await fetch('/api/register', {
+      const response = await fetch('http://localhost:5000/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
