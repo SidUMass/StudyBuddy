36ce19a1acd6086f3ff541420fc3df1b2dd79c79
Anushka Trehan <atrehan@umass.edu>
2023-11-16 21:10:13 -0500

    anushka's test cases 2

--- studybuddydb/database/tests.py
+++ studybuddydb/database/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-
 # Create your tests here.
 from django.test import TestCase
 from .models import CustomUser
@@ -11,7 +10,6 @@ class CustomUserModelTest(TestCase):
     def test_user_creation(self):
         user = CustomUser.objects.get(username="testuser")
         self.assertEqual(user.email, "test@example.com")
-
 
 from django.urls import reverse
 from django.test import TestCase, Client
@@ -28,3 +26,28 @@ class UserRegistrationTest(TestCase):
         })
         self.assertEqual(response.status_code, 200)
         self.assertTrue(CustomUser.objects.filter(username='newuser').exists())
+
+class CustomUserModelMethodTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username="testuser", email="test@example.com", password="testpassword123")
+
+    def test_some_custom_method(self):
+        # Assuming you have a custom method in CustomUser model
+        result = self.user.some_custom_method()
+        self.assertEqual(result, expected_result)
+
+class UserAPITest(TestCase):
+    def test_user_creation_api(self):
+        response = self.client.post('/api/users/', {'username': 'apiuser', 'password': 'testpass123', 'email': 'apiuser@example.com'})
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(CustomUser.objects.filter(username='apiuser').exists())
+
+class UserAuthenticationTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username='testuser', password='testpass123')
+
+    def test_login(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get('/some-protected-view/')
+        self.assertEqual(response.status_code, 200)
+
