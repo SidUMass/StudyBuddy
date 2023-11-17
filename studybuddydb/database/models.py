2af3eb41da347cfa0360546820afc2734eb387a2
Anushka Trehan <atrehan@umass.edu>
2023-11-16 20:42:46 -0500

    anushka's db stuff

--- studybuddydb/database/models.py
+++ studybuddydb/database/models.py
@@ -1,6 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User  # Importing the built-in User model
 from django.db import models
 
-class CustomUser(AbstractUser):
-    # Add additional fields if needed
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
